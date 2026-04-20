b1638d87211e5f13bda61913761d235d8bf5560d
prpkng <guiejo2020@gmail.com>
2026-04-20 01:04:59 -0300

    chore: organized spinning quad

--- Lemon/src/Lemon/Renderer/TestDXLayer.cpp
+++ Lemon/src/Lemon/Renderer/TestDXLayer.cpp
@@ -1,192 +1,60 @@
 #include "TestDXLayer.h"
+#include <d3dx12.h>
+#include <d3d12.h>
 #include <dxgi.h>
 #include <dxgi1_4.h>
-#include <d3d12.h>
-#include <d3d12shader.h>
 #include <d3dcompiler.h>
 
 #include "Platform/WindowsWindow.h"
 #include "SDL3/SDL_properties.h"
 #include "SDL3/SDL_video.h"
 
-#define CHECK(x, msg) {HRESULT hr = x; if (hr != S_OK) logHRError(hr, msg); }
-
 struct Vertex {
     float position[2];
     float color[3];
 };
-
-std::string HrToString(HRESULT hr)
-{
-    switch (hr)
-    {
-        // Common success
-        case S_OK: return "S_OK";
-
-        // DXGI errors
-        case DXGI_ERROR_DEVICE_REMOVED: return "DXGI_ERROR_DEVICE_REMOVED";
-        case DXGI_ERROR_DEVICE_HUNG: return "DXGI_ERROR_DEVICE_HUNG";
-        case DXGI_ERROR_DEVICE_RESET: return "DXGI_ERROR_DEVICE_RESET";
-        case DXGI_ERROR_DRIVER_INTERNAL_ERROR: return "DXGI_ERROR_DRIVER_INTERNAL_ERROR";
-        case DXGI_ERROR_INVALID_CALL: return "DXGI_ERROR_INVALID_CALL";
-        case DXGI_ERROR_WAS_STILL_DRAWING: return "DXGI_ERROR_WAS_STILL_DRAWING";
-        case DXGI_ERROR_FRAME_STATISTICS_DISJOINT: return "DXGI_ERROR_FRAME_STATISTICS_DISJOINT";
-        case DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE: return "DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE";
-        case DXGI_ERROR_NOT_CURRENTLY_AVAILABLE: return "DXGI_ERROR_NOT_CURRENTLY_AVAILABLE";
-        case DXGI_ERROR_NOT_FOUND: return "DXGI_ERROR_NOT_FOUND";
-        case DXGI_ERROR_MORE_DATA: return "DXGI_ERROR_MORE_DATA";
-        case DXGI_ERROR_UNSUPPORTED: return "DXGI_ERROR_UNSUPPORTED";
-
-        // D3D12 specific
-        case D3D12_ERROR_ADAPTER_NOT_FOUND: return "D3D12_ERROR_ADAPTER_NOT_FOUND";
-        case D3D12_ERROR_DRIVER_VERSION_MISMATCH: return "D3D12_ERROR_DRIVER_VERSION_MISMATCH";
-
-        // Generic HRESULTs
-        case E_FAIL: return "E_FAIL";
-        case E_INVALIDARG: return "E_INVALIDARG";
-        case E_OUTOFMEMORY: return "E_OUTOFMEMORY";
-        case E_NOTIMPL: return "E_NOTIMPL";
-        case E_POINTER: return "E_POINTER";
-
-        default:
-            break;
-    }
-
-    // Fallback: use Windows to get message
-    char* msgBuf = nullptr;
-    DWORD size = FormatMessageA(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER |
-        FORMAT_MESSAGE_FROM_SYSTEM |
-        FORMAT_MESSAGE_IGNORE_INSERTS,
-        nullptr,
-        hr,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPSTR)&msgBuf,
-        0,
-        nullptr);
-
-    std::ostringstream oss;
-
-    if (size && msgBuf)
-    {
-        oss << msgBuf;
-        LocalFree(msgBuf);
-    }
-    else
-    {
-        oss << "Unknown HRESULT";
-    }
-
-    // Append hex code (VERY useful)
-    oss << " (0x" << std::hex << hr << ")";
-
-    return oss.str();
-}
 
 void logHRError(HRESULT hr, std::string_view msg) {
     auto errorMsg = HrToString(hr);
     LM_CORE_FATAL("{0}: ERROR {1}", msg, errorMsg);
 }
 
-void setBlendState(D3D12_BLEND_DESC &blendDesc) {
-    blendDesc = {};
-
-    blendDesc.AlphaToCoverageEnable = FALSE;
-    blendDesc.IndependentBlendEnable = FALSE;
-
-    D3D12_RENDER_TARGET_BLEND_DESC defaultRenderTargetBlendDesc = {};
-    defaultRenderTargetBlendDesc.BlendEnable = FALSE;
-    defaultRenderTargetBlendDesc.LogicOpEnable = FALSE;
-    defaultRenderTargetBlendDesc.SrcBlend = D3D12_BLEND_ONE;
-    defaultRenderTargetBlendDesc.DestBlend = D3D12_BLEND_ZERO;
-    defaultRenderTargetBlendDesc.BlendOp = D3D12_BLEND_OP_ADD;
-    defaultRenderTargetBlendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
-    defaultRenderTargetBlendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
-    defaultRenderTargetBlendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
-    defaultRenderTargetBlendDesc.LogicOp = D3D12_LOGIC_OP_NOOP;
-    defaultRenderTargetBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-
-    for (UINT i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; i++) {
-        blendDesc.RenderTarget[i] = defaultRenderTargetBlendDesc;
-    }
-}
-
-void setRasterizerState(D3D12_RASTERIZER_DESC &rasterizerDesc) {
-    rasterizerDesc = {};
-
-    rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
-    rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
-    rasterizerDesc.FrontCounterClockwise = FALSE;
-    rasterizerDesc.DepthBias = D3D12_DEFAULT_DEPTH_BIAS;
-    rasterizerDesc.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
-    rasterizerDesc.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
-    rasterizerDesc.DepthClipEnable = TRUE;
-    rasterizerDesc.MultisampleEnable = FALSE;
-    rasterizerDesc.AntialiasedLineEnable = FALSE;
-    rasterizerDesc.ForcedSampleCount = 0;
-    rasterizerDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
-}
-
-void setDepthStencilState(D3D12_DEPTH_STENCIL_DESC &depthStencilDesc) {
-    depthStencilDesc = {};
-
-    depthStencilDesc.DepthEnable = FALSE;
-    depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
-    depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_GREATER;
-
-    depthStencilDesc.StencilEnable = FALSE;
-    depthStencilDesc.StencilReadMask = D3D12_DEFAULT_STENCIL_READ_MASK;
-    depthStencilDesc.StencilWriteMask = D3D12_DEFAULT_STENCIL_WRITE_MASK;
-
-    depthStencilDesc.FrontFace.StencilFailOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.FrontFace.StencilDepthFailOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.FrontFace.StencilPassOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.FrontFace.StencilFunc = D3D12_COMPARISON_FUNC_ALWAYS;
-
-    depthStencilDesc.BackFace.StencilFailOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.BackFace.StencilDepthFailOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.BackFace.StencilPassOp = D3D12_STENCIL_OP_KEEP;
-    depthStencilDesc.BackFace.StencilFunc = D3D12_COMPARISON_FUNC_ALWAYS;
-}
-
-TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX Layer") {
-
-    window = static_cast<Lemon::WindowsWindow*>(wnd.get());
-
-    SDL_PropertiesID props = SDL_GetWindowProperties(window->m_Handle);
-    HWND hwnd = (HWND)SDL_GetPointerProperty(props, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nullptr);
-    LM_CORE_ASSERT(hwnd, "Failed to retrieve HWND from SDL!");
-
+ID3D12Device * TestDXLayer::InitDevice() {
     // Create a rendering device
     ID3D12Device* device = nullptr;
-    CHECK(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device)), "Failed to create device");
+    ThrowIfFailed(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device)), "Failed to create device");
+    return device;
+}
 
+void TestDXLayer::InitCommandQueue(ID3D12Device *device) {
     // The command queue decides which order the command lists should execute. In our case, only one command list exists.
     commandQueue = nullptr;
     D3D12_COMMAND_QUEUE_DESC queueDesc = {};
     queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
     queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-    CHECK(device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue)), "Failed to create command queue");
+    ThrowIfFailed(device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue)), "Failed to create command queue");
 
     // The command allocator is used to allocate memory on the GPU for commands
     commandAllocator = nullptr;
-    CHECK(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)), "Failed to create command allocator");
-    CHECK(commandAllocator->Reset(), "Failed to reset command allocator");
+    ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)), "Failed to create command allocator");
+    ThrowIfFailed(commandAllocator->Reset(), "Failed to reset command allocator");
 
     // The command list is used to store a list of commands we wish to execute on the GPU
     commandList = nullptr;
-    CHECK(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList)), "Failed to create command list");
-    CHECK(commandList->Close(), "Failed to close command list");
+    ThrowIfFailed(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList)), "Failed to create command list");
+    ThrowIfFailed(commandList->Close(), "Failed to close command list");
+}
 
+void TestDXLayer::InitSwapchain(HWND hwnd) {
     // Helper factory to create the swap chain
     IDXGIFactory4* factory = nullptr;
-    CHECK(CreateDXGIFactory1(IID_PPV_ARGS(&factory)), "Failed to create DXGI factory");
+    ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&factory)), "Failed to create DXGI factory");
 
     // Create the swap chain
     DXGI_SWAP_CHAIN_DESC swapChainDesc = {
         .BufferDesc = DXGI_MODE_DESC {
-            .Width = wnd->GetWidth(),
-            .Height = wnd->GetHeight(),
+            .Width = window->GetWidth(),
+            .Height = window->GetHeight(),
             .Format = DXGI_FORMAT_R8G8B8A8_UNORM
         },
         .SampleDesc = DXGI_SAMPLE_DESC { .Count = 1},
@@ -198,39 +66,45 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     };
 
     IDXGISwapChain* tempSwapChain = nullptr;
-    CHECK(factory->CreateSwapChain(commandQueue, &swapChainDesc, &tempSwapChain), "Failed to create swap chain");
+    ThrowIfFailed(factory->CreateSwapChain(commandQueue, &swapChainDesc, &tempSwapChain), "Failed to create swap chain");
 
     // Cast swap chain to IDXGISwapChain3 to leverage the lastest features
     swapChain = {};
-    CHECK(tempSwapChain->QueryInterface(IID_PPV_ARGS(&swapChain)), "Failed to cast swap chain");
+    ThrowIfFailed(tempSwapChain->QueryInterface(IID_PPV_ARGS(&swapChain)), "Failed to cast swap chain");
     tempSwapChain->Release();
     tempSwapChain = nullptr;
+}
 
+void TestDXLayer::InitRenderTargets(ID3D12Device *device) {
     // Memory descriptor heap to store render target views (RTV). Descriptor describes how to interperate resource memory
     rtvHeap = nullptr;
     D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
     rtvHeapDesc.NumDescriptors = 2;
     rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-    CHECK(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap)), "Failed to create descriptor heap");
+    ThrowIfFailed(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap)), "Failed to create descriptor heap");
 
     rtvIncrementSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
     {
         D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle(rtvHeap->GetCPUDescriptorHandleForHeapStart());
         for (UINT i = 0; i < 2; i++) {
-            CHECK(swapChain->GetBuffer(i, IID_PPV_ARGS(&renderTargets[i])), "Failed to get swapchain buffer");
+            ThrowIfFailed(swapChain->GetBuffer(i, IID_PPV_ARGS(&renderTargets[i])), "Failed to get swapchain buffer");
 
             device->CreateRenderTargetView(renderTargets[i], nullptr, rtvHandle);
             rtvHandle.ptr += rtvIncrementSize;
         }
     }
+}
 
+void TestDXLayer::InitSync(ID3D12Device *device) {
     // Fence is used to synchronize the CPU with the GPU, so they don't touch the same memory at the same time
     fence = nullptr;
     fenceValue = 0;
-    CHECK(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)), "Failed to create fence");
+    ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)), "Failed to create fence");
 
     fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+}
 
+void TestDXLayer::InitShaderPipeline(ID3D12Device *device) {
     //Root signature is like have many object buffers and textures we want to use when drawing.
     //For our rotating triangle, we only need a single constant that is going to be our angle
 
@@ -252,8 +126,8 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
 
     ID3DBlob* signatureBlob = nullptr;
     ID3DBlob* errorBlob = nullptr;
-    CHECK(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob), "Failed to serialize root signature")
-    CHECK(device->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature)), "Failed to create root signature")
+    ThrowIfFailed(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob), "Failed to serialize root signature");
+    ThrowIfFailed(device->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature)), "Failed to create root signature");
 
     if (signatureBlob) {
         signatureBlob->Release();
@@ -267,10 +141,10 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
 
     ID3DBlob* vertexShader = nullptr;
     ID3DBlob* pixelShader = nullptr;
-    CHECK(D3DCompileFromFile(L"shader.hlsl", nullptr, nullptr, "VSMain",
-        "vs_5_0", 0, 0, &vertexShader, nullptr), "Failed to compile vertex shader");
-    CHECK(D3DCompileFromFile(L"shader.hlsl", nullptr, nullptr, "PSMain", "ps_5_0",
-        0, 0, &pixelShader, nullptr), "Failed to compile pixel shader");
+    ThrowIfFailed(D3DCompileFromFile(L"shader.hlsl", nullptr, nullptr, "VSMain",
+                                     "vs_5_0", 0, 0, &vertexShader, nullptr), "Failed to compile vertex shader");
+    ThrowIfFailed(D3DCompileFromFile(L"shader.hlsl", nullptr, nullptr, "PSMain", "ps_5_0",
+                                     0, 0, &pixelShader, nullptr), "Failed to compile pixel shader");
 
     // Pipeline state
     D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
@@ -282,12 +156,11 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     psoDesc.PS.pShaderBytecode = pixelShader->GetBufferPointer();
     psoDesc.PS.BytecodeLength = pixelShader->GetBufferSize();
 
-    setBlendState(psoDesc.BlendState);
-
     psoDesc.SampleMask = UINT_MAX;
 
-    setRasterizerState(psoDesc.RasterizerState);
-    setDepthStencilState(psoDesc.DepthStencilState);
+    psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+    psoDesc.BlendState      = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+    psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
 
     static D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
         { "POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
@@ -307,13 +180,15 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     psoDesc.SampleDesc.Quality = 0;
 
     pipelineState = nullptr;
-    CHECK(device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState)), "Failed to create pipeline state");
+    ThrowIfFailed(device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState)), "Failed to create pipeline state");
 
     vertexShader->Release();
     vertexShader = nullptr;
     pixelShader->Release();
     pixelShader = nullptr;
+}
 
+void TestDXLayer::InitBuffers(ID3D12Device *device) {
     static Vertex quadVertices[] = {
         { { -0.5f,  0.5f }, { 1.0f, 0.0f, 0.0f } }, // v0 (top-left)
         { {  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f } }, // v1 (top-right)
@@ -340,14 +215,14 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     resourceDesc.SampleDesc.Count = 1;
     resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 
-    CHECK(device->CreateCommittedResource(
-        &heapProps,
-        D3D12_HEAP_FLAG_NONE,
-        &resourceDesc,
-        D3D12_RESOURCE_STATE_GENERIC_READ,
-        nullptr,
-        IID_PPV_ARGS(&vertexBuffer)
-    ), "Failed to create vertex buffer");
+    ThrowIfFailed(device->CreateCommittedResource(
+                      &heapProps,
+                      D3D12_HEAP_FLAG_NONE,
+                      &resourceDesc,
+                      D3D12_RESOURCE_STATE_GENERIC_READ,
+                      nullptr,
+                      IID_PPV_ARGS(&vertexBuffer)
+                  ), "Failed to create vertex buffer");
 
     void* pData = nullptr;
     D3D12_RANGE readRange = {0, 0}; // We won't read from it
@@ -378,14 +253,14 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     resourceDesc.SampleDesc.Count = 1;
     resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 
-    CHECK(device->CreateCommittedResource(
-        &heapProps,
-        D3D12_HEAP_FLAG_NONE,
-        &resourceDesc,
-        D3D12_RESOURCE_STATE_GENERIC_READ,
-        nullptr,
-        IID_PPV_ARGS(&indexBuffer)
-    ), "Failed to create index buffer");
+    ThrowIfFailed(device->CreateCommittedResource(
+                      &heapProps,
+                      D3D12_HEAP_FLAG_NONE,
+                      &resourceDesc,
+                      D3D12_RESOURCE_STATE_GENERIC_READ,
+                      nullptr,
+                      IID_PPV_ARGS(&indexBuffer)
+                  ), "Failed to create index buffer");
 
     pData = nullptr;
     readRange = {0, 0};
@@ -400,16 +275,39 @@ TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX L
     indexBufferView.Format = DXGI_FORMAT_R16_UINT; // because uint16_t
 }
 
+TestDXLayer::TestDXLayer(std::unique_ptr<Lemon::Window>& wnd) : Layer("Test DX Layer") {
+
+    window = static_cast<Lemon::WindowsWindow*>(wnd.get());
+
+    SDL_PropertiesID props = SDL_GetWindowProperties(window->m_Handle);
+    HWND hwnd = (HWND)SDL_GetPointerProperty(props, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nullptr);
+    LM_CORE_ASSERT(hwnd, "Failed to retrieve HWND from SDL!");
+
+    ID3D12Device *device = InitDevice();
+
+    InitCommandQueue(device);
+
+    InitSwapchain(hwnd);
+
+    InitRenderTargets(device);
+
+    InitSync(device);
+
+    InitShaderPipeline(device);
+
+    InitBuffers(device);
+}
+
 TestDXLayer::~TestDXLayer() {
 }
 
 void TestDXLayer::OnUpdate() {
     static UINT triangleAngle = 0;
 
-    CHECK(commandAllocator->Reset(), "Failed to reset command allocator");
+    ThrowIfFailed(commandAllocator->Reset(), "Failed to reset command allocator");
 
     // Record commands to draw a triangle
-    CHECK(commandList->Reset(commandAllocator, nullptr), "Failed to reset command list");
+    ThrowIfFailed(commandList->Reset(commandAllocator, nullptr), "Failed to reset command list");
 
     UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
 
@@ -458,19 +356,19 @@ void TestDXLayer::OnUpdate() {
         commandList->ResourceBarrier(1, &barrier);
     }
 
-    CHECK(commandList->Close(), "Failed to close command list");
+    ThrowIfFailed(commandList->Close(), "Failed to close command list");
 
     ID3D12CommandList* commandLists[] = { commandList };
     commandQueue->ExecuteCommandLists(1, commandLists);
 
-    CHECK(swapChain->Present(1, 0), "Failed to present swap chain");
+    ThrowIfFailed(swapChain->Present(1, 0), "Failed to present swap chain");
 
     //Wait on the CPU for the GPU frame to finish
     const UINT64 currentFenceValue = ++fenceValue;
-    CHECK(commandQueue->Signal(fence, currentFenceValue), "Failed to signal the fence");
+    ThrowIfFailed(commandQueue->Signal(fence, currentFenceValue), "Failed to signal the fence");
 
     if (fence->GetCompletedValue() < currentFenceValue) {
-        CHECK(fence->SetEventOnCompletion(currentFenceValue, fenceEvent), "Failed to set fence completion");
+        ThrowIfFailed(fence->SetEventOnCompletion(currentFenceValue, fenceEvent), "Failed to set fence completion");
         WaitForSingleObject(fenceEvent, INFINITE);
     }
 
--- Lemon/src/Lemon/Renderer/TestDXLayer.h
+++ Lemon/src/Lemon/Renderer/TestDXLayer.h
@@ -10,12 +10,99 @@
 #include "Lemon/Window.h"
 #include "Platform/WindowsWindow.h"
 
+inline std::string HrToString(HRESULT hr)
+{
+    switch (hr)
+    {
+        // Common success
+        case S_OK: return "S_OK";
+
+        // DXGI errors
+        case DXGI_ERROR_DEVICE_REMOVED: return "DXGI_ERROR_DEVICE_REMOVED";
+        case DXGI_ERROR_DEVICE_HUNG: return "DXGI_ERROR_DEVICE_HUNG";
+        case DXGI_ERROR_DEVICE_RESET: return "DXGI_ERROR_DEVICE_RESET";
+        case DXGI_ERROR_DRIVER_INTERNAL_ERROR: return "DXGI_ERROR_DRIVER_INTERNAL_ERROR";
+        case DXGI_ERROR_INVALID_CALL: return "DXGI_ERROR_INVALID_CALL";
+        case DXGI_ERROR_WAS_STILL_DRAWING: return "DXGI_ERROR_WAS_STILL_DRAWING";
+        case DXGI_ERROR_FRAME_STATISTICS_DISJOINT: return "DXGI_ERROR_FRAME_STATISTICS_DISJOINT";
+        case DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE: return "DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE";
+        case DXGI_ERROR_NOT_CURRENTLY_AVAILABLE: return "DXGI_ERROR_NOT_CURRENTLY_AVAILABLE";
+        case DXGI_ERROR_NOT_FOUND: return "DXGI_ERROR_NOT_FOUND";
+        case DXGI_ERROR_MORE_DATA: return "DXGI_ERROR_MORE_DATA";
+        case DXGI_ERROR_UNSUPPORTED: return "DXGI_ERROR_UNSUPPORTED";
+
+        // D3D12 specific
+        case D3D12_ERROR_ADAPTER_NOT_FOUND: return "D3D12_ERROR_ADAPTER_NOT_FOUND";
+        case D3D12_ERROR_DRIVER_VERSION_MISMATCH: return "D3D12_ERROR_DRIVER_VERSION_MISMATCH";
+
+        // Generic HRESULTs
+        case E_FAIL: return "E_FAIL";
+        case E_INVALIDARG: return "E_INVALIDARG";
+        case E_OUTOFMEMORY: return "E_OUTOFMEMORY";
+        case E_NOTIMPL: return "E_NOTIMPL";
+        case E_POINTER: return "E_POINTER";
+
+        default:
+            break;
+    }
+
+    // Fallback: use Windows to get message
+    char* msgBuf = nullptr;
+    DWORD size = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr,
+        hr,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPSTR)&msgBuf,
+        0,
+        nullptr);
+
+    std::ostringstream oss;
+
+    if (size && msgBuf)
+    {
+        oss << msgBuf;
+        LocalFree(msgBuf);
+    }
+    else
+    {
+        oss << "Unknown HRESULT";
+    }
+
+    // Append hex code (VERY useful)
+    oss << " (0x" << std::hex << hr << ")";
+
+    return oss.str();
+}
+
 class TestDXLayer : public Lemon::Layer {
 public:
+    ID3D12Device *InitDevice();
+
+    void InitCommandQueue(ID3D12Device *device);
+
+    void InitSwapchain(HWND hwnd);
+
+    void InitRenderTargets(ID3D12Device *device);
+
+    void InitSync(ID3D12Device *device);
+
+    void InitShaderPipeline(ID3D12Device *device);
+
+    void InitBuffers(ID3D12Device *device);
+
     explicit TestDXLayer(std::unique_ptr<Lemon::Window>& wnd);
     ~TestDXLayer() override;
 
     void OnUpdate() override;
+
+    inline void ThrowIfFailed(HRESULT hr, std::string_view msg)
+    {
+        if (FAILED(hr))
+            throw std::runtime_error(std::string(msg) + ": " + HrToString(hr));
+    }
 private:
     Lemon::WindowsWindow* window;
 
